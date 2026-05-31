38d64cdf02144b40207ae8275fce9b77cc7acc2f
Aryan Singh <Aryanladdusingh@gmail.com>
2026-05-31 12:45:07 +0530

    update codes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,7 +7,16 @@ const userModel = require("../models/user.model")
  */
 
 
-async function registerUserController(req,res) { }
+async function registerUserController(req,res) {
+
+    const { username, email, password } = req.body
+
+    if(!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide username, email and password"
+        })
+     }
+ }
 
 module.exports = {
     registerUserController
